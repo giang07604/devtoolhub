c997668730e1b295e6219dea3a6bd3bfe0029d3b
Nguyễn Trường Giang <111481996+giang07604@users.noreply.github.com>
2025-11-29 12:31:06 +0700

    Implement fetching and displaying student codes
    Fetches student codes from the server and updates the table with active students' codes.

---
+++ fix_report_asm.js
@@ -0,0 +1,33 @@
+var studentCode = [];
+var class_id = document.getElementById("select_class_id").value;
+fetch(
+  "https://lms.languagehub.vn/teacher/student?key_search=&class_id=" + class_id + "&per_page=50"
+)
+  .then((response) => response.text())
+  .then((data) => {
+    const parser = new DOMParser();
+    const htmlDocument = parser.parseFromString(data, "text/html");
+    const tableRows = htmlDocument.querySelectorAll(".custom-table tbody tr");
+    tableRows.forEach((row) => {
+      if (
+        row
+          .querySelector(".text-left:nth-child(2) span:nth-child(2)")
+          .textContent.trim() == "Active"
+      ) {
+        var code = row
+          .querySelector(".text-left:nth-child(1)")
+          .textContent.trim();
+        studentCode.push(code);
+      }
+    });
+    var tbodyRows = document.querySelectorAll("tbody tr");
+    var theadRows = document.querySelectorAll("thead tr");
+    theadRows[0].querySelector("th:nth-child(3)").textContent = "Student Code";
+    theadRows[1].style.borderBottom = "1px solid #ececec";
+    tbodyRows.forEach(function (row, index) {
+      row.querySelector("td:nth-child(3)").textContent = studentCode[index];
+    });
+  })
+  .catch((error) => {
+    console.error("Đã xảy ra lỗi:", error);
+  });
